0a8026ca9d7aab2c8678931b61a739d0f7da87ff
Federallyapproved <51273285+Federallyapproved@users.noreply.github.com>
2024-09-23 20:31:06 -0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import requests
+import os
+
+# Initialize the bot with a command prefix
+bot = commands.Bot(command_prefix='/')
+
+# Azure Function URL
+AZURE_FUNCTION_URL = 'https://therefereebot.azurewebsites.net/api/referee_func?code=Uq01KCalwfazdyjd2ahkUXdjcL9RScITv6jlAek3YPuUAzFueUCGJw%3D%3D'
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+@bot.command(name='TheRef')
+async def the_ref(ctx, game_name: str):
+    response = requests.get(AZURE_FUNCTION_URL, params={'game_name': game_name})
+    if response.status_code == 200:
+        await ctx.send(response.text)
+    else:
+        await ctx.send("Sorry, there was an error retrieving the game rules.")
+
+# Run the bot with token
+bot.run(os.getenv('MTI4NzkyMDUxMDQxOTg2MTU2NQ.GWXNVv.CcqpAgF5zkAbDDkw_3m77o3r965N2ldiLVERyA'))
